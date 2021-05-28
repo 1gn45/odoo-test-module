041fa991362df2351c5f410f9685aae5588ff788
Ignas Jurgaitis <ignas.jur@gmail.com>
2021-05-28 23:56:36 +0300

    lesson 27 error still to be solved

--- models/models.py
+++ models/models.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields
 
+
+class Car(models.Model):
+    _name = "car.car"
+
+    name = fields.Char(string='name')
+    horse_power = fields.Integer(string="Horse Power")
+    door_number = fields.Integer(string=" string Doors number")
+    driver_id   = fields.Many2one("res.partner", string="Driver")
+    parking_id  = fields.Many2one("parking.parking" "Parking id")
+
+class Parking(models.Model):
+    _name = "parking.parking"
+
+    name = fields.Char(string='Name')
+    car_ids = fields.One2many("car.car", "parking_id", string="Cars")
 
 # class testas(models.Model):
 #     _name = 'testas.testas'
